1f59eee941601bb47331d6103416ce99373723d2
racoon-rac <racoonlover26@gmail.com>
2023-08-29 04:38:28 +0900

    Update MainController.java

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -3,20 +3,24 @@ package fr.christophetd.log4shell.vulnerableapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@RestController
+@Controller
 public class MainController {
 
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
+    // public String index(@RequestHeader("X-Api-Version") String apiVersion) {
+    //     logger.info("Received a request for API version " + apiVersion);
+    //     return "こんにちは、世界！";
+    // }
     @GetMapping("/")
-    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
-        logger.info("Received a request for API version " + apiVersion);
-        return "こんにちは、世界！";
+    public String index() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+          "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
     }
 
 }
